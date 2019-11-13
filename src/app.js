8dcf42c7a50e7ca3aabbc83babdb6ae35d39bca4
Terrence Persaud Jr <terrence.persaud@gmail.com>
2019-11-13 02:44:49 -0600

    finished module 4

--- src/app.js
+++ src/app.js
@@ -3,23 +3,13 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+let { accounts, users, writeJSON } = require('./data');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-
-const accountData = fs.readFileSync(
-  path.join(__dirname, 'json', 'accounts.json'), 'UTF8'
-);
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(
-  path.join(__dirname, 'json', 'users.json'), 'UTF8'
-);
-
-const users = JSON.parse(userData);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Account Summary', accounts });
@@ -53,13 +43,7 @@ app.post('/transfer', (req, res) => {
   accounts[from].balance -= amount;
   accounts[to].balance += amount;
 
-  const accountsJSON = JSON.stringify(accounts, null, 4);
-
-  fs.writeFileSync(
-    path.join(__dirname, 'json', 'accounts.json'),
-    accountsJSON,
-    'UTF8'
-  );
+  writeJSON();
 
   res.render('transfer', { message: 'Transfer Completed' });
 });
@@ -74,13 +58,7 @@ app.post('/payment', (req, res) => {
   accounts.credit.balance -= amount;
   accounts.credit.available += amount;
 
-  const accountsJSON = JSON.stringify(accounts, null, 4);
-
-  fs.writeFileSync(
-    path.join(__dirname, 'json', 'accounts.json'),
-    accountsJSON,
-    'UTF8'
-  );
+  writeJSON();
 
   res.render('payment', { message: 'Payment Successful', accounts: accounts.credit });
 });
